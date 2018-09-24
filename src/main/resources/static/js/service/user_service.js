6bef7d200fa3eaf2d78a9c1197b5538ad0280e49
sartan21 <43236751+sartan21@users.noreply.github.com>
2018-09-24 12:53:16 +0200

    Update user_service.js

--- src/main/resources/static/js/service/user_service.js
+++ src/main/resources/static/js/service/user_service.js
@@ -2,7 +2,7 @@
 
 angular.module('myApp').factory('UserService', ['$http', '$q', function($http, $q){
 
-    var REST_SERVICE_URI = 'http://localhost:8080/SpringBootHibernateAngular/user/';
+    var REST_SERVICE_URI = 'http://myapp-myapp.2886795314-80-simba02.environments.katacoda.com/SpringBootHibernateAngular/user/';
 
     var factory = {
         fetchAllUsers: fetchAllUsers,
